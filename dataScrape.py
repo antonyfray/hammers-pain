d76460ff72252ac2e3547e5f9593456ead8c928a
Ant <antonyfray@hotmail.co.uk>
2020-06-22 21:00:52 +0100

    Add getClubHistory function and basic bar plot of club vs days

--- dataScrape.py
+++ dataScrape.py
@@ -4,87 +4,102 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def getDaysInj(injPageSoup):
-    daysInjLst = []
+    daysInjList = []
     DaysInjured = injPageSoup.find_all("td", {"class": "rechts"})
     for i in DaysInjured:
         if "days" in i.text:
-            daysInjLst.append(i.text.split()[0])
+            daysInjList.append(i.text.split()[0])
 
-    for i in daysInjLst:
+    for i in daysInjList:
         print("Days inj = {}".format(i))
-    return daysInjLst
+    return daysInjList
 
 def getClubInjFor(injPageSoup):
     # wappen is German for coat of arms i.e. club badge. All of the img sources have this in their endpoint
-    clubInjLst = []
+    clubInjList = []
     ClubInjuredFor = injPageSoup.find_all("img", src=lambda x: x and 'wappen' in x)
     for i in ClubInjuredFor:
-        clubInjLst.append(i["alt"])
+        clubInjList.append(i["alt"])
         # current code also picks up current club as an extra entry (badge at top of screen) so we need to remove that
-    del(clubInjLst[0])
+    del(clubInjList[0])
 
-    for i in clubInjLst:
+    for i in clubInjList:
         print("Club inj for = {}".format(i))
-    return clubInjLst
+    return clubInjList
 
 def getInjTypes(injPageSoup):
-    typeInjLst = []
+    typeInjList = []
     # Many classes have hauptlink in name but we want an exact match
     # most recent injury is also highlighted red (bg_rot_20) so we need to collect that too
     TypeOfInjury = injPageSoup.find_all(lambda tag: tag.name == 'td' and (tag.get('class') == ['hauptlink'] or tag.get('class') == ['hauptlink','bg_rot_20']))
     for i in TypeOfInjury:
         print(i.text)
-        typeInjLst.append(i.text)
+        typeInjList.append(i.text)
 
-    for i in typeInjLst:
+    for i in typeInjList:
         print("Type of injury = {}".format(i))
-    return typeInjLst
+    return typeInjList
 
 def getTransferDates(tfPageSoup):
-    tfDatesLst = []
+    tfDatesList = []
     tfDates = tfPageSoup.find_all(lambda tag: tag.name == 'td' and (tag.get('class') == ['zentriert','hide-for-small']))
     for i in tfDates:
         if "," in i.text:
             # need to convert date format into datetime object for processing
-            tfDatesLst.append(datetime.strptime(i.text, '%b %d, %Y'))
+            tfDatesList.append(datetime.strptime(i.text, '%b %d, %Y'))
 
     print("Transfer dates:")
-    for i in tfDatesLst:
+    for i in tfDatesList:
         print(i)
-    return tfDatesLst
+    return tfDatesList
 
 def getDaysAtClub(tfPageSoup):
-    daysAtClubLst = []
+    daysAtClubList = []
     print("Days spent at each club")
     # Need to determine time spent at current club
     currentDate = datetime.now()
-    for i in range(0,len(tfDatesLst)):
+    for i in range(0,len(tfDatesList)):
         if i is 0:
-            days = (currentDate - tfDatesLst[i]).days
+            days = (currentDate - tfDatesList[i]).days
             print(days)
-            daysAtClubLst.append(days)
+            daysAtClubList.append(days)
         else:
-            days = (tfDatesLst[i-1]-tfDatesLst[i]).days
+            days = (tfDatesList[i-1]-tfDatesList[i]).days
             print(days)
-            daysAtClubLst.append(days)
-    return daysAtClubLst
+            daysAtClubList.append(days)
+    return daysAtClubList
+
+def getClubHistory(tfPageSoup):
+    clubHistList = []
+    clubHistory = tfPageSoup.find_all("img", src=lambda x: x and 'wappen' in x)
+
+    for i in clubHistory:
+        clubHistList.append(i["alt"])
+    # current code also picks up current club as an extra entry (badge at top of screen) so we need to remove that
+    del(clubHistList[0])
+
+    return clubHistList[1::2]
 
 def getClubTransfers(tfPageSoup):
-    clubTrnsLst = []
+    # TODO start of this is duplicated with the above. Should have one function that returns a list/tuple of lists
+    clubTrnsList = []
     clubTransfers = tfPageSoup.find_all("img", src=lambda x: x and 'wappen' in x)
 
     for i in clubTransfers:
-        clubTrnsLst.append(i["alt"])
+        clubTrnsList.append(i["alt"])
         # current code also picks up current club as an extra entry (badge at top of screen) so we need to remove that
-    del(clubTrnsLst[0])
+    del(clubTrnsList[0])
 
-    for i in range(0,len(clubTrnsLst),2):
-        if (i+1)==len(clubTrnsLst):
+    for i in range(0,len(clubTrnsList),2):
+        if (i+1)==len(clubTrnsList):
             break
-        print('From {} to {}'.format(clubTrnsLst[i],clubTrnsLst[i+1]))
-    return clubTrnsLst
+        print('From {} to {}'.format(clubTrnsList[i],clubTrnsList[i+1]))
+    return clubTrnsList
 
 if __name__ == "__main__":
 
@@ -108,22 +123,26 @@ if __name__ == "__main__":
     tfPageSoup = BeautifulSoup(tfPageTree.content, 'html.parser')
 
     # Get and store nDaysInjured
-    daysInjLst = getDaysInj(injPageSoup)
+    daysInjList = getDaysInj(injPageSoup)
 
     # Get and store club player is signed to while injured
-    clubInjLst = getClubInjFor(injPageSoup)
+    clubInjList = getClubInjFor(injPageSoup)
 
     # Get and store injury type
-    typeInjLst = getInjTypes(injPageSoup)
+    typeInjList = getInjTypes(injPageSoup)
 
     # Get date of transfer
-    tfDatesLst = getTransferDates(tfPageSoup)
+    tfDatesList = getTransferDates(tfPageSoup)
 
     # Get days spent at each club
-    daysAtClubLst = getDaysAtClub(tfPageSoup)
+    daysAtClubList = getDaysAtClub(tfPageSoup)
 
     # Get clubs transferred between
     clubTrnsList = getClubTransfers(tfPageSoup)
+
+    # Get list of clubs (to correspond to daysAtClub)
+    clubHistList = getClubHistory(tfPageSoup)
+
 
 # TODO figure out how this time normalisation will work - are we doing total days at club or by season?
 # We need to use the above to determine how long a player played for each club
@@ -133,33 +152,34 @@ if __name__ == "__main__":
 # Naive approach would be just add together all the days the player spent at the club, but that fails to account for e.g. one injury having a knockon
 # effect (chronology of injuries).
 
-    multiLst = []
-    multiLst.append(typeInjLst)
-    multiLst.append(clubInjLst)
-    multiLst.append(daysInjLst)
-    print("type len = {}".format(len(typeInjLst)))
-    print("club len = {}".format(len(clubInjLst)))
-    print("days len = {}".format(len(daysInjLst)))
-    if not all(len(i) == len(multiLst[0]) for i in multiLst):
+    multiList = []
+    multiList.append(typeInjList)
+    multiList.append(clubInjList)
+    multiList.append(daysInjList)
+    print("type len = {}".format(len(typeInjList)))
+    print("club len = {}".format(len(clubInjList)))
+    print("days len = {}".format(len(daysInjList)))
+    if not all(len(i) == len(multiList[0]) for i in multiList):
         sys.exit("Arrays are not all the same length. This is a problem.")
 
+    # TODO plot in different file, read in dataframe and do it that way
+    plt.bar(clubHistList,daysAtClubList, label=playerName)
+    plt.xlabel("Club")
+    plt.ylabel("Days Spent at Club")
+    plt.legend(loc="upper right")
+    fig1 = plt.gcf() # get current figure - before we save locally
+    plt.show()
+    plt.draw()
+    fig1.savefig("clubVsDays.png")
 
-#
-# Manipulate arrays etc, plot stuff, do calculates of relevant stats
-#
 
-
-# TODO Store
-# 1) Total number of days injured - done (just need to sum daysInjArr)
-# 2) Total number of individual injury occurences
-# 3) Club player was signed to when injured - done
-# 4) Type of injury (is it the same one every time?) - done
-# 5) Date of transfers between clubs (from https://www.transfermarkt.co.uk/andy-carroll/transfers/spieler/48066) - done
+# TODO
+# Date of transfers between clubs (from https://www.transfermarkt.co.uk/andy-carroll/transfers/spieler/48066) - done
 # This is so we can normalise time injured against time playing for the club
 # Date format is e.g. Aug 31, 2013 so will need to be able to convert this into dateformat for time comparison
-# 6) Date of injury
-# TODO Check
-# 1) Ensure arrs are all the same length
+
+# TODO create a dataframe from what we scrape for use with seaborn
+
 
 # TODO Functionality
 # 1) If there are multiple pages of injury history, need a way to know how many pages of info there are
